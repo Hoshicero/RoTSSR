fbfc111e148501b59cf15e91cf4641203e040d19
Hoshicero <43017617+Hoshicero@users.noreply.github.com>
2019-02-25 16:28:39 -0500

    ALPA v1.2

---
+++ RoTSSR/Class1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public  class StationCreator
+    {
+
+        /*Title: StationCreator
+             *Arguments:
+             * (int)Rm_Amt -- The amountof rooms requested by the operator.
+             * Variables:
+             * (int)Fl_ControlNumMin -- The least amount of floors desired
+             * (int)Fl_ControlNumMax -- The max amount of floors desired
+             * (int)DK_ControlNumMin -- The least amount of Decks desired
+             * (int)Dk_ControlNumMax -- The Most amount of Decks desired.
+             * (int)Rm_Divisior -- Room Divisor is the total number of desired floors for the gameboard. 
+             * (int)Rm_PerFloor -- Is the calculated result of the division 
+         */
+            
+
+               /***************CONTROL VARIABLES****************/
+       public int Fl_ControlNumMin { get; set;}
+       public  int Fl_ControlNumMax { get; set;}
+       public  int Rm_ControlNumMin { get; set;}
+       public int Rm_ControlNumMax { get;set;}
+       public int Dk_ControlNumMin { get; set;}
+       public int Dk_ControlNumMax { get; set;}
+       public int Rm_Divisor { get; set;}
+       public int Rm_PerFloor { get; set;} /*By dividing the total amount of rooms by the total number of floors, we can arrive at the total number of rooms per floor.*/
+       public int Rm_Amt { get; set;}
+       public int Leastnumber{get; set;}//The least number a room can be at any given time.
+       public StationCreator() { } /*Constructor*\
+
+        /***************CONTAINER VARIABLES*******************************/
+
+        /*
+             public StationCreator(int Flmin, int Flmax, int Rmmin, int Rmmax, int Dkmin, int Dkmax, int RmDiv, int Rmperfl, int Rm_Amt, int lnum)
+             {
+
+                 Fl_ControlNumMin = Flmin;
+                 Fl_ControlNumMax = Flmax;
+                 Rm_ControlNumMin = Rmmin;
+                 Rm_ControlNumMax = Rmmax;
+                 Dk_ControlNumMin = Dkmin;
+                 Dk_ControlNumMax = Dkmax;
+                 Rm_Divisor = RmDiv;
+                 Rm_PerFloor = Rmperfl;
+                 Leastnumber = lnum;
+
+             }
+
+     */
+
+
+        /************************COUNTER VARIABLES*************************************/
+
+        int Rm_Tracker { get; set; } /*Floortracker is a counter that keeps track of how many rooms have been made, it will be used to trigger the next floor*/
+        int Fl_Monitor { get; set; } /*Keeps track of the current level of floor It starts out as three because the numbers go up in ascending order*/
+        int Dk_Monitor { get; set; } /*Keeps track of the current Deck*/
+        int Fl_Tracker { get; set; }
+
+
+
+        /*****************************************************************************/
+
+
+       
+
+        String nNeighborCalculations(String HolderRoom)
+        {
+            /*"X" Is a default value that I set to act as a gatekeeper*/
+
+            if (HolderRoom == "X")
+            {
+
+                /*IF YOU ARE ON THE TOP DECK ON THE TOP FLOOR, YOU HAVE NO NORTH*/
+
+                if ((Dk_Monitor == Dk_ControlNumMin) && (Fl_Monitor == Fl_ControlNumMin))
+                {
+
+
+                    return "X";
+
+
+                }
+
+                /*OR IF you are on the highest floor of your deck,
+                Your N_Neighbor will be equal to a string that is a concatenation of the result of your current Deck Minus one,
+                 The Max Floor Control value, and your current room number. 
+                 Ex: CurrentRoom is B123, B123's North Neighbor would be: (*B*(2) - 1) = (1)*A*), Fl_Max = 3, and 123 giving a total value of : A3123 *
+                 */
+
+                else if (Fl_Monitor == Fl_ControlNumMin)
+                {
+
+
+                    return String.Concat(Dk_Retriever(Dk_Monitor - 1), Fl_ControlNumMin);
+
+                }
+
+
+                else
+                    return String.Concat(Dk_Retriever(Dk_Monitor), (Fl_Monitor - 1));
+                //OTHERWISE YOUR NORTH NEIGHBOR CONSISTS OF YOUR DECK, YOUR FLOOR - 1, AND YOUR CURRENT ROOM NUMBER.
+
+            }
+
+            else
+                return null;
+
+
+        }
+
+        String sNeighborCalculations(String HolderRoom)
+
+        {
+            if (HolderRoom == "X")
+            {
+
+                /*IF YOU ARE THE LOWEST DECK ON THE LOWEST FLOOR, YOU HAVE NO SOUTH NEIGHBOR*/
+                if ((Dk_Monitor == Dk_ControlNumMax) && (Fl_Monitor == Fl_ControlNumMax))
+                {
+
+
+                    return "X";
+
+
+                }
+                /*IF YOU HAPPEN TO BE ON THE FLOOR OF A DECK RIGHT ABOVE THE TOP FLOOR OF ANOTHER DECK */
+                else if (Fl_Tracker == Fl_ControlNumMax)
+                {
+                    return String.Concat(Dk_Retriever(Dk_Monitor + 1), Fl_ControlNumMin);
+                }
+
+                else
+                    return String.Concat(Dk_Retriever(Dk_Monitor), (Fl_Monitor + 1));
+
+
+            }
+
+            else
+                return null;
+
+        }
+
+
+        public String wNeighborCalculations(String HolderRoom, int Lnumber)
+        {
+
+            if (HolderRoom == "X")
+            {
+
+                /*IF YOU ARE THE FIRST ROOM ON YOUR FLOOR, YOU HAVE NO EAST NEIGHBOR*/
+                if (Lnumber == Rm_ControlNumMin)
+                {
+
+
+                    return "X";
+
+                }
+
+
+                else
+                    return String.Concat(Dk_Retriever(Dk_Monitor), Fl_Monitor);
+
+
+            }
+            else return null;
+
+        }
+
+
+        public String eNeighborCalculations(String HolderRoom, int Rm_PerFloor)
+        {
+
+
+            if (HolderRoom == "X")
+            {
+
+                /*IF YOU ARE THE LAST ROOM ON YOUR FLOOR, YOU HAVE NO EAST NEIGHBOR*/
+                if (Rm_PerFloor == Rm_ControlNumMax)
+                {
+
+
+                    return "X";
+
+                }
+
+
+                else
+                    return String.Concat(Dk_Retriever(Dk_Monitor), Fl_Monitor);
+
+
+            }
+
+            else return null;
+
+
+        }
+
+
+
+
+char Dk_Retriever(int num)
+        {
+
+            switch (num)
+            {
+
+
+                case 1:
+                    return 'A';
+
+                case 2:
+                    return 'B';
+                case 3:
+                    return 'C';
+
+                default:
+                    return 'X';
+
+
+            }
+
+        }
+
+    }
+}
--- RoTSSR/Program.cs
+++ RoTSSR/Program.cs
@@ -6,256 +6,62 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
-    class MainClass
-    {
+    public class MClass : StationCreator {
+        public static object Fl_control { get; private set; }
+
         public static void Main(string[] args)
+
+ /*
+ Title: Main
+ Type: Void
+ Function: To facilitate the execution of appropriate class methods.
+ Variables: 
+ *(int)RoomSize -- The amount of Rooms a user would like to be included into the final board. 
+ *(int)IndexHolder -- A variable designed to help the Hashtable keep track of it's indexes.
+ *(Hashtable) valueTable The Hash table that stores a key and value.
+*/
         {
 
-            int RoomSize = 81;
+           
+            int RoomSize = 100;
+            MClass.Rm_ControlNumMin = 100;
+            int IndexHolder = 0; 
+            IDictionary<String, int> valueTable = new Dictionary<String, int>();
+            Room Roomtester = new Room();
+            
 
             List<Room> RoomPopulator = new List<Room>();
 
             RoomPopulator = StationCreator(RoomSize);
 
-            /*   
-              for(int x = 0; x <= RoomSize; x++)
-           {
+           
 
-               System.Console.WriteLine(RoomPopulator[x].GetNeighbor_S().ToString());
-               System.Console.WriteLine(RoomPopulator[x].GetNeighbor_E().ToString());
-               System.Console.WriteLine(RoomPopulator[x].GetNeighbor_W().ToString());
+          
 
 
+            Console.ReadLine();
 
-           }
-
-           */
-
-            /*
-
-                  Station Creator's purpose is to take parameters denoting amount of rooms. It will then randoml assign door statuses to various 
-                  corners of a room based on a randomly generated value. 
-
- It does this by firstly creating a List of Rooms, secondly inducting a forloop that iterates through the random number generator sequences, assigning door values 
-
-
-
-
-             */
 
             List<Room> StationCreator(int Amt)
-
-            /* Station Creator is a long ass method. I may at a later date break it up into smaller pieces, but for right now the goal is to dynamically automate the population of decks, and floors.
-               I will do this by reading in the amt from the Amt variable, dividing the read-in amt by the Roomdivisor, and then proceeding through a series of loops that will check each room as it's being created,
-               and associate the proper neighbor credentials to it. I will have a few control systems in place. The first will monitor decks, and as soon as it recieves word that three floors are filled it will trigger itself to begin another deck
-               The second control system is the floor system which will monitor floors, as soon as a floor reaches the value stored in Roomamt it will reset itself to zero, increment the floor control, and continue. 
-         */
-            {
-
-                /***************CONTROL VARIABLES****************/
-                int Fl_ControlNumMin = 1; /*The least amount of floors desired.*/
-                int Fl_ControlNumMax = 3;/*The amount of  desired*/
-                int Dk_ControlNumMin = 1; /*The least amount of Decks desired*/
-                int Dk_ControlNumMax = 3; /*The Most amount of Decks desired. */
-                int Rm_Divisor = 9;/*Room Divisor is the total amount of floors on the gameboard. For our game the board consists of three decks with three floors each, thus netting a total 9 floors*/
-                int Rm_Amount = Amt / Rm_Divisor; /*By dividing the toatal of Amt by the total number of floors, we can arrive at the total number of rooms requested for the gameboard.*/
-                const int Maxnumber = 50; //The maximum number a room can be at any given time.
-                const int Leastnumber = 0;//The least number a room can be at any given time.
-
-
-                /***************CONTAINER VARIABLES*******************************/
+         
+                
+        
 
 
 
-                /************************COUNTER VARIABLES*************************************/
+                            if (Dk_Retriever(HolderRoom.Get_Dk()) != 'X')
+                            {   
+                                RoomList.Add(new Room(Na: HolderRoom.Get_N(), NN: HolderRoom.NorthNeighbor, NE: HolderRoom.EastNeighbor, NW: HolderRoom.WestNeighbor, NS: HolderRoom.SouthNeighbor));
+                                valueTable.Add(HolderRoom.Get_N(), IndexHolder);
+                                //Console.Write(HolderRoom.Get_N());
+                                //Console.Write(IndexHolder);
 
-                int Rm_Tracker = 0; /*Floortracker is a counter that keeps track of how many rooms have been made, it will be used to trigger the next floor*/
-                int Fl_Monitor = 3; /*Keeps track of the current level of floor It starts out as three because the numbers go up in ascending order*/
-                int Dk_Monitor = Dk_ControlNumMax; /*Keeps track of the current Deck*/
-                int Fl_Tracker = Fl_ControlNumMax;
-
-
-
-                /*****************************************************************************/
-
-
-
-
-                List<Room> RoomList = new List<Room>();
-
-                Random rnd = new Random();
-
-                for (int x = 0; x < Rm_Divisor; x++)
-                {
-                    Dk_Monitor = Dk_ControlNumMax;
-
-                    for (int y = 0; y < Dk_ControlNumMax; y++)
-                    {
-                        
-
-                        for (int z = 0; z < Rm_Amount; z++)
-                        {
-                            /*A new Current_Room Object is created: int, char, int*/
-                            Current_Room HolderRoom = new Current_Room(Fl_Tracker, Dk_Monitor, Rm_Tracker, Dk_Retriever(Dk_Monitor).ToString());
-
-
-                            /*Now it's time to initialize a method that has Current_Room check itself to see if it matches any special room requirements. */
-
-                            HolderRoom.Fl_Minmaxcheck(Fl_ControlNumMin, Fl_ControlNumMax);
-                            HolderRoom.Dk_Minmaxcheck(Dk_ControlNumMin, Dk_ControlNumMax);
-
-
-
-                            /****************************NORTH FLOOR CALCULATIONS*******************************/
-
-                            /*"X" Is a default value that I set to act as a gatekeeper*/
-
-
-                            if (HolderRoom.NorthNeighbor == "X")
-                            {
-
-                                /*IF YOU ARE ON THE TOP DECK ON THE TOP FLOOR, YOU HAVE NO NORTH*/
-
-                                if (HolderRoom.Dk_Min && HolderRoom.Fl_Min)
-                                {
-
-
-                                    HolderRoom.NorthNeighbor = "X";
-
-
-                                }
-
-                                /*OR IF you are on the highest floor of your deck,
-                                Your N_Neighbor will be equal to a string that is a concatenation of the result of your current Deck Minus one,
-                                 The Max Floor Control value, and your current room number. 
-                                 Ex: CurrentRoom is B123, B123's North Neighbor would be: (*B*(2) - 1) = (1)*A*), Fl_Max = 3, and 123 giving a total value of : A3123 *
-                                 */
-
-                                else if (HolderRoom.Fl_Min)
-                                {
-
-
-                                    HolderRoom.NorthNeighbor = String.Concat(Dk_Retriever(HolderRoom.Get_Dk() - 1), Fl_ControlNumMax, HolderRoom.CurrentRoom);
-
-                                }
-
-                                //OTHERWISE YOUR NORTH NEIGHBOR CONSISTS OF YOUR DECK, YOUR FLOOR - 1, AND YOUR CURRENT ROOM NUMBER. 
-
-                                else
-                                    HolderRoom.NorthNeighbor = String.Concat(Dk_Retriever(HolderRoom.Get_Dk()), (HolderRoom.Get_Fl() - 1), HolderRoom.CurrentRoom);
-
+                                IndexHolder++;
+                                
+                                // System.Console.WriteLine(String.Concat(HolderRoom.Get_N(), "-", "(", HolderRoom.NorthNeighbor, ")", "(", HolderRoom.EastNeighbor, ")", "(", HolderRoom.WestNeighbor, ")", "(", HolderRoom.SouthNeighbor, ")"));
                             }
 
-
-
-                            /****************************SOUTH FLOOR CALCULATIONS*******************************/
-
-
-                            if (HolderRoom.SouthNeighbor == "X")
-                            {
-
-                                /*IF YOU ARE THE LOWEST DECK ON THE LOWEST FLOOR, YOU HAVE NO SOUTH NEIGHBOR*/
-                                if (HolderRoom.Dk_Max && HolderRoom.Fl_Max)
-                                {
-
-
-                                    HolderRoom.SouthNeighbor = "X";
-
-
-                                }
-
-                                else if (HolderRoom.Fl_Max)
-                                {
-                                    HolderRoom.SouthNeighbor = String.Concat(Dk_Retriever(HolderRoom.Get_Dk() + 1), Fl_ControlNumMin, HolderRoom.CurrentRoom);
-                                }
-
-                                else
-                                    HolderRoom.SouthNeighbor = String.Concat(Dk_Retriever(HolderRoom.Get_Dk()), (HolderRoom.Get_Fl() + 1), HolderRoom.CurrentRoom);
-
-
-
-
-                            }
-
-                            /****************************EAST FLOOR CALCULATIONS*******************************/
-
-                            if (HolderRoom.EastNeighbor == "X")
-                            {
-
-                                /*IF YOU ARE THE LAST ROOM ON YOUR FLOOR, YOU HAVE NO EAST NEIGHBOR*/
-                                if (HolderRoom.CurrentRoom == Maxnumber)
-                                {
-
-
-                                    HolderRoom.EastNeighbor = "X";
-
-                                }
-
-
-                                else
-                                    HolderRoom.EastNeighbor = String.Concat(Dk_Retriever(HolderRoom.Get_Dk()), HolderRoom.Get_Fl(), (HolderRoom.CurrentRoom + 1));
-
-
-                            }
-
-
-                            /****************************WEST FLOOR CALCULATIONS*******************************/
-
-
-                            if (HolderRoom.WestNeighbor == "X")
-                            {
-
-                                /*IF YOU ARE THE FIRST ROOM ON YOUR FLOOR, YOU HAVE NO EAST NEIGHBOR*/
-                                if (HolderRoom.CurrentRoom == Leastnumber)
-                                {
-
-
-                                    HolderRoom.WestNeighbor = "X";
-
-                                }
-
-
-                                else
-                                    HolderRoom.WestNeighbor = String.Concat(Dk_Retriever(HolderRoom.Get_Dk()), HolderRoom.Get_Fl(), (HolderRoom.CurrentRoom - 1));
-
-
-                            }
-
-
-                            /**********************************************************************************/
-
-
-
-
-                            //RoomList.Add(new Room(Na: "YAH", NN: "YAH", NE: "YAH", NW: "YAH", NS: "YAH"));
-                            RoomList.Add(new Room(Na: HolderRoom.Get_N(), NN: HolderRoom.NorthNeighbor, NE: HolderRoom.EastNeighbor, NW: HolderRoom.WestNeighbor, NS: HolderRoom.SouthNeighbor));
-
-                            System.Console.WriteLine(String.Concat(HolderRoom.Get_N(), "-", "(", HolderRoom.NorthNeighbor, ")", "(", HolderRoom.EastNeighbor, ")", "(", HolderRoom.WestNeighbor, ")", "(", HolderRoom.SouthNeighbor, ")"));
-
-
-                            /*                    
-                               int N_wall = rnd.Next(1, 7);
-                               int S_wall = rnd.Next(1, 7);
-                               int E_wall = rnd.Next(1, 7);
-                               int W_wall = rnd.Next(1, 7);
-
-
-                               Boundary Boundpoop = new Boundary();
-                               Boundpoop = RoomList[0].GetBoundary('N');
-                               System.Console.WriteLine("Counter is" + Counter);
-                               System.Console.WriteLine("Roomlist is" + Boundpoop.GetPosition());
-                               /* Random Number Generator sequences */
-
-
-
-
-
-
-
-
-
-
-
+                          
 
                             Rm_Tracker++;
 
@@ -309,6 +115,53 @@ namespace ConsoleApp1
             }
 
         }
+
+
+
+
+        public static Room Rm_Get(String ind, List<Room> rm_List, IDictionary<String, int>hTable)
+        //Searches Room list for a Room by first searching the k&v pair in the Dictionary object called "htable" by
+        // the String key variable called "ind" and using the int value from that lookup as the index for the room to return.
+        {
+            var vTable = new Dictionary<string, int>(hTable);
+     
+        
+            int xmFile = 0;
+            xmFile = hTable[ind];
+            return rm_List[xmFile];
+
+
+
+        }
+
+
+
+        public static void Screen_Draw(List<Room> Materials, IDictionary<string, int> tTable)
+        { 
+            IDictionary<String, int> valueTable = new Dictionary<String, int>(tTable);
+            // var tempTable = new Dictionary<string, int>(tTable);
+            int Columns = 11;
+            const int rows = 0;
+            for (int x = 0; x <= 3; x++)
+            {
+                for (int y = 0; y <= Columns; y++)
+                {
+                    Console.Write($"{Columns,rows}", "Hello");
+
+
+
+
+
+                }
+
+
+
+
+            }
+
+        }
+
+      
     }
 
 
@@ -339,6 +192,7 @@ namespace ConsoleApp1
 
         public void Fl_Minmaxcheck(int Min, int max)
         {
+            //If the current floor is equal to the minimum floor
             if (Floor == Min)
             {
 
@@ -359,6 +213,7 @@ namespace ConsoleApp1
 
         public void Dk_Minmaxcheck(int Min, int max)
         {
+            //If The current Deck is equal to the minimum Deck
             if (Deck == Min)
             {
 
@@ -378,7 +233,7 @@ namespace ConsoleApp1
 
 
 
-
+        //Constructor
         public Current_Room(int Fl, int Dk, int Ro, String Dk_N)
         {
 
@@ -414,7 +269,11 @@ namespace ConsoleApp1
 
 
         }
+
+    
     }
+
+   
 }
 
 
@@ -424,19 +283,6 @@ namespace ConsoleApp1
 
 
 
-    
-
-
- 
-
-
-            
-
-
-
-                     
-                            
-                
 
 
 
@@ -446,4 +292,16 @@ namespace ConsoleApp1
 
 
 
-           
+
+
+
+
+
+
+
+
+
+
+
+
+
